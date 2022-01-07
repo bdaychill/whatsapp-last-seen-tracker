5c2b56cb2b1fcbd2d68ab266dc9ec92137be8ad9
CHILLBRO <83193083+bdaychill@users.noreply.github.com>
2022-01-07 22:01:29 +0530

    Update wls.js

--- wls.js
+++ wls.js
@@ -3,87 +3,71 @@
 // ONLINE OR LAST SEEN TRACK SECTION
 
 let lastSeenData;
-//document.getElementById
+let lastSeenOld;
 
+let className = document.querySelector("span[title=online]")
+let isOnline = function (className) {
+try {
+className = document.querySelector("span[title=online]")
+lastSeenData = className.title
 
-let isOnline = function(className) {
-
-
-    try {
-        lastSeenData = document.querySelector(className).textContent
-
-        //console.log(lastSeenData)
-
-        if (lastSeenData == "online") {
-            return true;
-        }
-        return false;
-    } catch (e) {
-        //console.log(e.message)
-        return false;
-    }
-
+if (lastSeenData == "online") {
+  //console.log('Online', new Date())
+  return true;
 }
+return false;
+} catch (e) {
+return false;
+}
+};
 
 let timeout;
 let onlineTrace = [];
 let timeInTimeOut = [];
 
-
-
-
 let onlineBool = [];
 
-let showOnlineTrace = function() {
-    let data = "";
-    for (let i = 0; i < onlineTrace.length; i++) {
-        let s = '[From ' + onlineTrace[i][0] + ' To ' + onlineTrace[i][1] + ']'
+let showOnlineTrace = function () {
+let data = "";
+for (let i = 0; i < onlineTrace.length; i++) {
+let s = "[From " + onlineTrace[i][0] + " To " + onlineTrace[i][1] + "]";
 
-        data += s + '\n';
-
-    }
-    // return data;
-    console.log(data);
-
+data += s + "\n";
 }
+// return data;
+console.log("=>", data);
+};
 
+let checkLastSeen = function (className) {
+let status = isOnline(className);
 
-let checkLastSeen = function(className) {
+onlineBool.push(status);
+let lastOnlineBoolIndex = onlineBool.length - 1;
 
-    let status = isOnline(className)
+let t = new Date();
 
-    onlineBool.push(status)
-    let lastOnlineBoolIndex = onlineBool.length - 1
+//console.log(onlineBool[lastOnlineBoolIndex-1])
 
-    let t = new Date()
+if (status) {
+time = new Date();
 
-    //console.log(onlineBool[lastOnlineBoolIndex-1])
-
-    if (status) {
-
-
-        time = t.getDate() + '-' + (t.getMonth() + 1) + '-' + t.getFullYear() + ' ' + t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds()
-
-        if (lastOnlineBoolIndex == 0) {
-            timeInTimeOut.push(time)
-        } else if (onlineBool[lastOnlineBoolIndex - 1] == false) {
-            timeInTimeOut.push(time)
-        }
-
-    } else {
-        if (onlineBool[lastOnlineBoolIndex - 1] == true) {
-            timeInTimeOut.push(time)
-            onlineTrace.push(timeInTimeOut)
-            timeInTimeOut = []
-        }
-    }
-
+if (lastOnlineBoolIndex == 0) {
+  timeInTimeOut.push(time);
+} else if (onlineBool[lastOnlineBoolIndex - 1] == false) {
+  timeInTimeOut.push(time);
 }
-
-let listen = function(className) {
-
-    checkLastSeen(className);
-    setTimeout(listen, 1000,className);
+} else {
+if (onlineBool[lastOnlineBoolIndex - 1] == true) {
+timeInTimeOut.push(time);
+onlineTrace.push(timeInTimeOut);
+timeInTimeOut = [];
 }
+}
+};
+
+let listen = function (className) {
+checkLastSeen(className);
+setTimeout(listen, 1000, className);
+};
 
 listen(className);
